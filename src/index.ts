68e0284f6e5e0db5704331ac15c67d489a8d70b2
unknown <rafaelods1994@gmail.com>
2025-08-09 17:12:46 +1200

    [RS] enabling cross origin

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 import { sequelize } from './models/db';
 import { setupSwagger } from './swagger';
 
 const app = express();
+app.use(cors({
+  origin: 'http://localhost:3001',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+
 app.use(express.json());
 
 // Swagger docs
